18b0606aeea3b997986841315295002f3ad84303
Shuping <shuping.ruan@gmail.com>
2016-10-17 15:16:33 -0400

    update ode_RL_design.m, add .gitignore

--- ode_RL_design.m
+++ ode_RL_design.m
@@ -31,19 +31,21 @@ W = nan(N, T);
 M = nan(N, T);
 D = nan(N, T);
 
-% The initial values W0 and M0 for each patient are generated independently
-% from uniform (0, 2) deviates.
-% In general, you can generate N random numbers in the interval (a,b) with the formula r = a + (b-a).*rand(N,1).
+% The initial values W0 and M0 for each patient are generated iid
+% from uniform (0, 2). D0 iid ~ uniform (0.5, 1), Dt iid ~ uniform(0,1)
+% In general, you can generate N random numbers in the interval (a,b) 
+% with the formula r = a + (b-a).*rand(N,1).
 % t = 0
 W(:,1) = 0 + (2 - 0) .* rand(N,1);
 M(:,1) = 0 + (2 - 0) .* rand(N,1); 
 D(:,1) = 0.5 + (1 - 0.5) .* rand(N,1);
 D(:,2:T) = 1 + (1 - 0) .* rand(N,T-1);
+
 for t = 1:T
     Mt1 = [M(:,t), M(:,1)];
     Wt1 = [W(:,t), W(:,1)];
     dWt = a1 .*  max(Mt1, [], 2) + b1 .* (D(:,t) - d1);
-    dMt = (a2 .* max(Wt1, [], 2) - b2 .* (D(:,t) - d2)).* (M(:,t) > 0);
+    dMt = ( a2 .* max(Wt1, [], 2) - b2 .* (D(:,t) - d2) ).* ( M(:,t) > 0 );
     W(:, t+1) = W(:, t) + dWt;
     M(:, t+1) = M(:, t) + dMt;
 end
